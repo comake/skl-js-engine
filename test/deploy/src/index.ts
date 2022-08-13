d8cf6a4c1594e9dc0a39e57a543b37b9bca9bf72
Adler Faulkner <aif28@cornell.edu>
2022-08-13 13:16:26 -0700

    test: dont catch script errors

--- test/deploy/src/index.ts
+++ test/deploy/src/index.ts
@@ -10,16 +10,12 @@ async function run(): Promise<void> {
   const env = { TICKETMASTER_APIKEY: process.env.TICKETMASTER_APIKEY! };
   const schema = await frameAndCombineSchemas(schemas, env);
   const skql = new Skql({ schema });
-  try {
-    const eventsCollection = await skql.do.getEvents({
-      account: 'https://skl.standard.storage/data/TicketmasterAccount1',
-      city: 'Atlanta',
-      pageSize: 20,
-    });
-    console.log(eventsCollection.records);
-  } catch (err: unknown) {
-    console.log('script errored', err);
-  }
+  const eventsCollection = await skql.do.getEvents({
+    account: 'https://skl.standard.storage/data/TicketmasterAccount1',
+    city: 'Atlanta',
+    pageSize: 20,
+  });
+  console.log(eventsCollection.records);
 }
 
 run();
