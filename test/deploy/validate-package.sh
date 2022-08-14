38f9b43b0124023e341389382f3e56791bfda6a0
Adler Faulkner <aif28@cornell.edu>
2022-08-14 15:54:46 -0700

    test: specify package name in npm pack

--- test/deploy/validate-package.sh
+++ test/deploy/validate-package.sh
@@ -6,7 +6,8 @@ TEST_NAME="Deployment test: packaged module"
 echo "$TEST_NAME - Building and installing package"
 pwd
 
-npm pack --loglevel warn --pack-destination ./test/deploy &&
+npm pack @comake/skql-js-engine --loglevel warn --pack-destination test/deploy
+
 cd test/deploy
 
 npm install
