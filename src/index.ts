073f28b29dac0879197cff3cadfa374bbf5068a1
Adler Faulkner <aif28@cornell.edu>
2022-12-09 15:55:49 -0500

    feat(sparql): add missing files

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,23 @@
 // Mapping
 export * from './mapping/Mapper';
 
-// Storage
-export * from './storage/MemoryQueryAdapter';
-export * from './storage/QueryAdapter';
+// Storage/Operator
+export * from './storage/operator/In';
 
+// Storage/Sparql
 export * from './storage/sparql/SparqlQueryAdapter';
 export * from './storage/sparql/SparqlQueryBuilder';
 export * from './storage/sparql/SparqlUpdateBuilder';
 export * from './storage/sparql/VariableGenerator';
 
+// Storage
+export * from './storage/FindOperator';
+export * from './storage/FindOptionsTypes';
+export * from './storage/MemoryQueryAdapter';
+export * from './storage/QueryAdapter';
+
 // Util
+export * from './util/TripleUtil';
 export * from './util/Types';
 export * from './util/Util';
 export * from './util/Vocabularies';
