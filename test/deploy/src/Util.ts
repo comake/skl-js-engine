10c2e9a3eb68857dc2653363c67f6d37c0b33038
Adler Faulkner <aif28@cornell.edu>
2023-11-02 14:37:37 -0700

    feat(sparql-in-memory): fix package integration test

--- test/deploy/src/Util.ts
+++ test/deploy/src/Util.ts
@@ -6,7 +6,7 @@ export async function frameAndCombineSchemas(
   filePaths: string[],
   env: Record<string, string> = {},
 ): Promise<Entity[]> {
-  const schemas = await Promise.all(
+  const nestedSchemas = await Promise.all(
     filePaths.map(async(filePath: string): Promise<jsonld.NodeObject[]> => {
       let schema = await fs.readFile(filePath, { encoding: 'utf8' });
       Object.keys(env).forEach((envVar: string): void => {
@@ -15,7 +15,8 @@ export async function frameAndCombineSchemas(
       return await jsonld.expand(JSON.parse(schema));
     }),
   );
-  const expandedSchema = schemas.flat();
-  const framedSchema = await jsonld.frame(expandedSchema, {});
-  return framedSchema['@graph'] as Entity[];
+  const schemas = nestedSchemas.flat();
+  return await Promise.all(
+    schemas.map((schema): Promise<Entity> => jsonld.compact(schema, {}) as Promise<Entity>),
+  );
 }
